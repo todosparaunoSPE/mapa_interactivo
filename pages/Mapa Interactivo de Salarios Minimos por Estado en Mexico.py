d78af3ca1b88d007a33a8d9cfcd818014f7b8738
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-11-22 10:11:08 -0600

    Update Mapa Interactivo de Salarios Minimos por Estado en Mexico.py

--- pages/Mapa Interactivo de Salarios Minimos por Estado en Mexico.py
+++ pages/Mapa Interactivo de Salarios Minimos por Estado en Mexico.py
@@ -13,6 +13,22 @@ import json
 # Configuración de la página: debe ser el primer comando
 st.set_page_config(page_title="Mapa de Salarios por Estado", layout="wide")
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 # Datos de salarios mínimos por estado
 data = {
     "Estado": [
